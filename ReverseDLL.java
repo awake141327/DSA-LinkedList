92a76bb72e1c2a9d4e3073a45b3129d670ffae85
Amaan <82905413+awake141327@users.noreply.github.com>
2024-04-07 08:56:26 +0000

    ReverseDLL

---
+++ ReverseDLL.java
@@ -0,0 +1,82 @@
+class ReverseDLL {
+
+    // Defining the Node Class
+    static class Node {
+        int val;
+        Node next;
+        Node back;
+                
+        Node (int val, Node next, Node back) {
+            this.val = val;
+            this.next = next;
+            this.back = back;
+        }
+        Node (int val) {
+            this.val = val;
+            this.next = null;
+            this.back = null;
+        }
+        Node() {  
+            this.val = 0;
+            this.next = null;
+            this.back = null;
+        }
+    }
+
+    // Converting an Array to a Doubly Linked List
+    public static Node convertToDLL(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node prev = head;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i], null, prev);
+            prev.next = temp;
+            prev = prev.next;
+        }
+        return head;
+    }
+
+    // Finding the Length of a Doubly Linked List
+    public static void printLength(Node head) {
+        Node temp = head;
+        int length = 0;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        System.out.println("Length = " + length);
+    }
+
+    // Reversing a Doubly Linked List
+    public static Node reversingDLL(Node head) {
+        // [null(prev), 1(temp), 2(front), 3,  null] => [null, 2, 1, null];
+        Node temp = head;
+        Node prev = null;
+        while(temp != null) {
+            prev = temp.back;
+            temp.back = temp.next;
+            temp.next = prev;
+            temp = temp.back;
+        }
+        return prev.back;
+    }
+
+    // Printing a Doubly Linked List
+    public static void print(Node head) {
+        System.out.print("DLL = ");
+        while(head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // Main Method
+    public static void main (String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        Node head = convertToDLL(arr);
+        print(head);
+        printLength(head);
+        head = reversingDLL(head);
+        print(head);
+    }
+}
